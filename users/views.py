9ed35e842e152eae5b003d3ef3fcc95ddee955f9
Naitik0909 <parmarnaitik0909@gmail.com>
2022-05-31 19:30:37 +0530

    Users patch API

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+import re
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -17,6 +18,9 @@ from .utils import is_doctor, is_nurse, is_patient
 from doctor.serializers import DoctorSerializer
 from patient.serializers import PatientSerializer
 from nurse.serializers import NurseSerializer
+from doctor.models import Doctor
+from nurse.models import Nurse
+from patient.models import Patient
 
 class RegisterUser(APIView):
     permission_classes = [AllowAny]
@@ -80,3 +84,62 @@ class GetUserDetails(generics.GenericAPIView):
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=status.HTTP_401_UNAUTHORIZED)
 
+    def put(self, request):
+        user = request.user
+
+        try:
+            role = request.data.get('role')
+            str_to_bool = {
+                "0": True,
+                "1": False
+            }
+
+            if role == "Patient":
+                patient = Patient.objects.get(user=user)
+                patient.user.first_name = request.data.get('first_name', '')
+                patient.user.last_name = request.data.get('last_name', '')
+                patient.phone = request.data.get('phone', '')
+                patient.address = request.data.get('address', '')
+                patient.age = request.data.get('age', '')
+                patient.dob = request.data.get('dob', '')
+                patient.gender = request.data.get('gender', '')
+                patient.blood_group = request.data.get('blood_group', '')
+                patient.aadhaar = request.data.get('aadhaar', '')
+                patient.emergeny_contact_name = request.data.get('emergeny_contact_name', '')
+                patient.emergeny_contact_phone = request.data.get('emergeny_contact_phone', '')
+                patient.emergency_contact_relation = request.data.get('emergency_contact_relation', '')
+                patient.save()
+            
+            elif role == "Doctor":
+                doctor = Doctor.objects.get(user=user)
+                doctor.user.first_name = request.data.get('first_name', '')
+                doctor.user.last_name = request.data.get('last_name', '')
+                doctor.phone = request.data.get('phone', '')
+                doctor.address = request.data.get('address', '')
+                doctor.age = request.data.get('age', '')
+                doctor.dob = request.data.get('dob', '')
+                doctor.gender = request.data.get('gender', '')
+                doctor.blood_group = request.data.get('blood_group', '')
+                doctor.aadhaar = request.data.get('aadhaar', '')
+                doctor.working_days = request.data.get('working_days', '')
+                doctor.specialization = request.data.get('specialization', '')
+                doctor.location = request.data.get('location', '')
+                doctor.save()
+            
+            elif role == "Nurse":
+                nurse = Nurse.objects.get(user=user)
+                nurse.user.first_name = request.data.get('first_name', '')
+                nurse.user.last_name = request.data.get('last_name', '')
+                nurse.phone = request.data.get('phone', '')
+                nurse.address = request.data.get('address', '')
+                nurse.age = request.data.get('age', '')
+                nurse.dob = request.data.get('dob', '')
+                nurse.gender = request.data.get('gender', '')
+                nurse.blood_group = request.data.get('blood_group', '')
+                nurse.aadhaar = request.data.get('aadhaar', '')
+                # nurse.is_day_shift = request.data.get('is_day_shift', '')
+                nurse.save()
+            return JsonResponse(status=status.HTTP_200_OK)
+            
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
